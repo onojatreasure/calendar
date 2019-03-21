bdcd2bedbc64a814975d88086f4d45b45c0d7729
Onoja Treasure <onojatreasure@gmail.com>
2019-03-21 14:12:20 +0100

    First release of Calender events app

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,29 @@
+import { CalendarComponent } from 'ionic2-calendar/calendar';
+import { Component, ViewChild, OnInit, Inject, LOCALE_ID } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { formatDate } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+import { HomePage } from './home.page';
+import { NgCalendarModule  } from 'ionic2-calendar';
+ 
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+    NgCalendarModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
